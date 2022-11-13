bbc1f9a466e133a7a498cf7a5d73ec48f84b53df
xeonmobius <refinedzeta@gmail.com>
2022-11-12 21:01:29 -0500

    reduce size of Coming SOON

--- pages/pastWork.js
+++ pages/pastWork.js
@@ -1,7 +1,7 @@
 export default function PastWork() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className='text-8xl raleway'>Coming soon!</h1>
+      <h1 className='text-6xl raleway'>Coming soon!</h1>
     </div>
   );
 }
